0000a1dc2d4c51c3776fa223bea95d8c186a75cd
maoamaya0420 <amayamao@amayamao-no-MacBook-Pro.local>
2016-09-03 09:05:10 +0900

    ねじ子の動きを制限する

--- Nejiko Run/Assets/NejikoBaseAssets/Script/NejikoController.cs
+++ Nejiko Run/Assets/NejikoBaseAssets/Script/NejikoController.cs
@@ -3,14 +3,21 @@ using System.Collections;
 
 public class NejikoController : MonoBehaviour {
 
+	const int MinLane = -2;
+	const int MaxLane = 2;
+	const float LaneWidth = 1.0f;
+
 	CharacterController controller;
 	Animator animator;
 
 	Vector3 moveDirection = Vector3.zero;
+	int targetLane;
 
 	public float gravity;
 	public float speedZ;
+	public float speedX;  //横方向スピードのパラメータ
 	public float speedJump;
+	public float accelerationZ;   //全身加速度のパラメータ
 
 	void Start()
 	{
@@ -22,6 +29,22 @@ public class NejikoController : MonoBehaviour {
 
 	void Update()
 	{
+		//デバッグ用
+		if (Input.GetKeyDown ("left"))
+			MoveToLeft ();
+		if (Input.GetKeyDown ("right"))
+			MoveToRight ();
+		if (Input.GetKeyDown ("space"))
+			Jump ();
+
+		//徐々に加速しZ方向に常に前進させる
+		float accelerateZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+		moveDirection.z = Mathf.Clamp (accelerateZ, 0, speedZ);
+
+		//X方向は目標のポジションまでの差分の割合で速度を計算
+		float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
+		moveDirection.x = ratioX * speedX;
+
 		//地上にいる場合のみ操作を行う
 		if (controller.isGrounded) {
 			//Inputを検知して前に進める
@@ -45,16 +68,40 @@ public class NejikoController : MonoBehaviour {
 		moveDirection.y -= gravity * Time.deltaTime;
 
 		//移動実行
-		Vector3 globalDirection = transform.TransformDirection(moveDirection);
+		Vector3 globalDirection = transform.TransformDirection (moveDirection);
 		controller.Move (globalDirection * Time.deltaTime);
 
 		//移動後設置してたらY方向の速度はリセットする
-		if(controller.isGrounded ) moveDirection.y = 0;
+		if (controller.isGrounded)
+			moveDirection.y = 0;
 
 		//速度が0以上なら走っているフラグをtrueにする
-		animator.SetBool("run", moveDirection.z > 0.0f);
+		animator.SetBool ("run", moveDirection.z > 0.0f);
 
 
-	
 	}
+		//左のレーンに移動を開始
+		public void MoveToLeft()
+	{
+		if (controller.isGrounded && targetLane > MinLane)
+			targetLane--;
+
+	}
+	//右のレーンに移動を開始
+	public void MoveToRight()
+	{
+		if (controller.isGrounded && targetLane < MaxLane)
+			targetLane++;
+	}
+
+	public void Jump()
+	{
+		if (controller.isGrounded) {
+			moveDirection.y = speedJump;
+
+			//ジャンプトリガーを設定
+			animator.SetTrigger ("jump");
+		}
+	}
+
 }
